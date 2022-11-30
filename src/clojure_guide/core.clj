23ae681e1eb36cb12147f1ba06212708acf3c538
Ivan Grishaev <ivan@grishaev.me>
2022-11-30 20:33:24 +0300

    updated

--- src/clojure_guide/core.clj
+++ src/clojure_guide/core.clj
@@ -1,1 +1,40 @@
 (ns clojure-guide.core)
+
+(time
+ (doseq [a [1 2 3]]
+   (let [b (* a a)]
+     (println b)
+     )
+   )
+ )
+
+(def mapping {
+   :name "Ivan"
+   :email "test@test.com"
+})
+
+(def numbers [
+  1,
+  2,
+  3
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#_ _
