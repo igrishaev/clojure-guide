5e3c371572351f4bda944f0603f7e1f254e373c9
Ivan Grishaev <ivan@grishaev.me>
2022-12-01 18:38:07 +0300

    updated

--- src/clojure_guide/core.clj
+++ src/clojure_guide/core.clj
@@ -128,20 +128,316 @@
   (translate :message/hello))
 
 
+(def storage
+  (atom {}))
+
+(defn set-item [idx item]
+  (swap! storage assoc idx item))
+
+(defn get-item [idx]
+  (get @storage idx))
+
+
+(def *storage
+  (atom {}))
+
+
+(def atom-storage (atom {}))
+(def a-storage (atom {}))
+
+(def storage! (atom {}))
+
+(swap! storage! ...)
+(reset! storage! ...)
+
+
+(let [rows! (transient [])]
+  (doseq ...
+    (conj! rows! item))
+  (persistent! rows!))
+
+
+(defn get-user [user-id]
+  (try
+    (jdbc/get-by-id *db* :users user-id)
+    (catch Throwable e
+      e)))
 
 
 
+(let [user (get-user 1)]
+  (if (throwable? user)
+    (do
+      (log/error ...)
+      (throw user))
+    (process user ...)))
+
+
+(defn get-user [user-id]
+  (try
+    (jdbc/get-by-id *db* :users user-id)
+    (catch Throwable e
+      {:type :error :message (ex-message e)})))
+
+
+(let [user (get-user 1)]
+  (if (error-map? user)
+    (do
+      (log/error ...)
+      (throw ...))
+    (process user ...)))
+
+
+(throw (ex-info "Some message" {:some "context"}))
 
 
 
+(defn error!
+
+  ([message]
+   (throw (ex-info message {})))
+
+  ([message data]
+   (throw (ex-info message data)))
+
+  ([message data cause]
+   (throw (ex-info message data cause))))
+
+
+(when-not user
+  (e/error! "User not found" {:id user-id}))
+
+
+(set! *assert* false)
+
+(let [data {:foo 1}]
+  (assert (get data :bar) "Bar is missing"))
+
+(set! *assert* false)
+
+(set! *assert* true)
+
+(defn process-user2
+  [user]
+  {:pre [(some? user)]}
+  (assoc user :some "field"))
+
+(process-user2 nil)
+
+
+(filter #(= (:id %) product-id) products)
+(update :body #(json/parse-string % ->kebab-case-keyword))
+
+
+(filter (fn [product]
+          (= (:id product) product-id))
+        products)
+
+(update :body json/parse-string ->kebab-case-keyword)
 
 
 
+(map inc numbers)
+
+(map str objects)
 
 
 
+(map (fn [{:keys [...]}]
+       (if this
+         (with-something
+           (log/info ...)
+           (do-that this))
+         (let [foo 1
+               bar 2]
+           (therwise this ...)))) entities)
 
 
+
+(defn process-entity [entity]
+  ...)
+
+(map process-entity entities)
+
+
+(for [entity entities]
+  (let [{:keys [...]}]
+    (return something)))
+
+
+
+(->> items
+     (map (partial process-item context current-time))
+     (filter (partial check-item event-type)))
+
+
+
+(def enumerate
+  (partial map-indexed vector))
+
+(enumerate [:a :b :c])
+
+([0 :a] [1 :b] [2 :c])
+
+
+(let [fn-process
+      (comp str inc abs)]
+  (fn-process -3))
+
+
+((comp str inc abs) -3)
+
+
+(map (comp this that more) [...])
+
+
+(defn make-xform [...]
+  (comp (map ...)
+        (filter ...)))
+
+
+(let [xform (make-xform)]
+  ...)
+
+
+(defn connect [host port & {:keys [log-level
+                                   log-file]}]
+  ...)
+
+(connect "localhost" 5432 :log-level "info" :log-file "log.txt")
+
+
+(defn connect
+  ([host port]
+   (connect host port nil))
+
+  ([host port {:keys [log-level log-file]}]
+   ...))
+
+
+(let [options {...}]
+  (connect host port options))
+
+
+(def response
+  {:status 200
+   :headers {,,,}
+   :body {:user {:email "test@test.com"
+                 :full-name "Ivan"}}})
+
+(let [{:keys [status body]}
+      response
+
+      {:keys [user]}
+      body
+
+      {:keys [email full-name]}
+      user]
+
+  (println email full-name))
+
+
+(let [{{{:keys [email full-name]} :user} :body}
+      response]
+  (println email full-name))
+
+
+(let [{:keys [:status :body]}
+      response]
+  ,,,)
+
+
+(def data
+  {:user/name "Ivan"
+   :event/name "Party"})
+
+
+(require 'cheshire.core)
+
+(cheshire.core/generate-string data)
+
+(let [{:user/keys [name]
+       :event/keys [name]} data]
+  (println name))
+;; Party
+
+
+(let [{user-name :user/name
+       event-name :event/name} data]
+  (println user-name "@" event-name))
+
+
+(let [user
+      {:user/name "..."
+       :user/email "..."
+       :user/dob "1985-..."
+       :user/active? true}]
+  ,,,)
+
+(let [user
+      {:name "..."
+       :email "..."
+       :dob "1985-..."
+       :active? true}]
+  ,,,)
+
+
+(let [row
+      {:user/name "..."
+       :user/email "..."
+       :user/dob "1985-..."
+       :user/active? true
+       :profile/user-id 1
+       :profile/created-at "..."
+       :profile/avatar "..."}]
+  ,,,)
+
+
+(let [row
+      {:user-name "..."
+       :user-email "..."
+       :user-dob "1985-..."
+       :user-active? true
+       :profile-user-id 1
+       :profile-created-at "..."
+       :profile-avatar "..."}]
+  ,,,)
+
+(let [row
+      {:user {:name "..."
+              :email "..."
+              :dob "1985-..."
+              :active? true}
+       :profile {:user-id 1
+                 :created-at "..."
+                 :avatar "..."}}]
+  ,,,)
+
+
+(require '[clojure.walk :as walk])
+
+(walk/postwalk
+ (fn [x]
+   (if (map?)
+     (update-keys x csk/->SCREAMING_SNAKE_CASE)
+     x))
+ data)
+
+
+
+(let [{:keys [user_id
+              user_name]} db-row]
+  (println user_id user_name))
+
+
+(let [{:keys [ResponseError
+              ResponseMessage]}
+      api-response
+
+      {:keys [Code Category]}
+      ResponseError]
+
+  (println Code Category ResponseMessage))
 
 
 
